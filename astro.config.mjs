00046905f03edf43a6fc21e2ae6dbaaf91f9a640
Sebastian Rodrigo <srodrigo@3clics.lat>
2025-12-29 13:24:04 -0300

    Make book clickable, add redirects, enable static build, update favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'static',
+  redirects: {
+    '/producto/la-nueva-violencia-moderna': '/producto',
+    '/producto/la-nueva-violencia-moderna/': '/producto',
+  },
 });
